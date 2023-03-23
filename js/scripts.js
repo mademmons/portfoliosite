e4057fc63c37537f9ee7104faa8084fd05982e57
mademmons <marissademmons@gmail.com>
2023-03-22 20:17:33 -0400

    Update scripts.js
    added toggle for menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
 console.log("Welcome! I appreciate you stopping by.")
+function menuToggle() {var x = document.getElementById('myNavtoggle'); {
+x.className += ' responsive';
+}else {
+    x.className = 'navtoggle';
+  }
+}
